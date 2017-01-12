8e0a7eb5e1b26b7afa52c61a04829b1c764deaf1
Anne Zhou <anne.zhou920@gmail.com>
2017-01-12 13:25:30 -0500

    try again

--- bubblesort.js
+++ bubblesort.js
@@ -1,5 +1,6 @@
-function swap(){
-
+function swap(num){
+  num++;
+  return num;
 }
 
 function comparisonCount(){
@@ -7,16 +8,23 @@ function comparisonCount(){
 }
 
 function bubbleSort(arr) {
-  var swap = true;
+  var swapCount = 0;
+  //
+  // while(){
+    for (var i = 0; i < arr.length - 1; i++){
+      var current = arr[i];
+      var next = arr[i+1];
 
-  for (var i = 0; i < arr.length - 1; i++){
-    var current = arr[i];
-    var next = arr[i+1];
-    if (next < current){
-      current = next;
-      next = arr[i+1];
+      if (next < current){
+        current = next;
+        next = arr[i];
+        swapCount = swap(swapCount);
+      }
+
     }
+    return arr;
   }
-}
+// }
 
 [5,2,4,3,1]
+[2,5]
